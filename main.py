89b11df9434c59fc08a9a69688ac27bfcc6f4151
Nizamuddin <nizamphoenix@gmail.com>
2020-02-12 00:02:54 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import preproc
 import utils
+import preproc
+import retrieve
+import train
+import inference
+
 doc_term_freq,vocab=create_doc_term_freq(filepath)
 invindex = InvertedIndex(vocab, doc_term_freqs)
 save_object(invindex, 'invindex_final.pkl')
 #invindex=load_object('invindex_final.pkl')               
+#compute BM25
+sample= invindex.num_docs()
+train_data = get_training_data_for_sample(sample)
+evidences = get_evidence(train_data)
