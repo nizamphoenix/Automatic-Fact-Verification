3de628fee82030d5fe8f4b5322909cc7bae6b966
Nizamuddin <nizamphoenix@gmail.com>
2020-02-11 23:31:44 +1100

    main file

---
+++ main.py
@@ -0,0 +1,6 @@
+import preprocess
+import misc
+doc_term_freq,vocab=create_doc_term_freq(filepath)
+invindex = InvertedIndex(vocab, doc_term_freqs)
+save_object(invindex, 'invindex_final.pkl')
+#invindex=load_object('invindex_final.pkl')               
