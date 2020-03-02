7396af116030d2bb48d9a9bb5f8b63678a96a369
Nizamuddin <nizamphoenix@gmail.com>
2020-03-02 22:52:36 +1100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import inference
 doc_term_freq,vocab=create_doc_term_freq(filepath)
 invindex = InvertedIndex(vocab, doc_term_freqs)
 save_object(invindex, 'invindex_final.pkl')
-#invindex=load_object('invindex_final.pkl')               
+invindex=load_object('invindex_final.pkl')               
 #compute BM25
 sample= invindex.num_docs()
 train_data = get_training_data_for_sample(sample)
