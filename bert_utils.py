9c096211d97fe8b0ec746e433dc3cbae7be5ad8c
Nizamuddin <nizamphoenix@gmail.com>
2020-04-24 20:48:54 +1000

    Update bert_utils.py

--- bert_utils.py
+++ bert_utils.py
@@ -8,8 +8,10 @@ import os
 from math import floor, ceil
 from transformers import *
 
-def return_id(str1, str2, truncation_strategy, length):
-
+def get_embed(str1, str2, truncation_strategy, length):
+        '''
+        reuturns embeddings of str1+str2
+        '''
         inputs = tokenizer.encode_plus(str1, str2,
             add_special_tokens=True,
             max_length=length,
@@ -23,24 +25,25 @@ def return_id(str1, str2, truncation_strategy, length):
         input_ids = input_ids + ([padding_id] * padding_length)
         input_masks = input_masks + ([0] * padding_length)
         input_segments = input_segments + ([0] * padding_length)
-        
         return [input_ids, input_masks, input_segments]
 
-def _convert_to_transformer_inputs(claim, evidence, tokenizer, max_sequence_length):
-    input_ids_q, input_masks_q, input_segments_q = return_id(
-        title + ' ' + question, None, 'longest_first', max_sequence_length)
+def _convert_to_transformer_inputs(claim, evidence_list, tokenizer, max_sequence_length):
+    input_ids_claim, input_masks_claim, input_segments_claim = get_embed(
+        claim, None, 'longest_first', max_sequence_length)
+
+    evidence= ''.join(evidence_list)
+    input_ids_evid, input_masks_evid, input_segments_evid = get_embed(
+        evidence, None, 'longest_first', max_sequence_length)
     
-    input_ids_a, input_masks_a, input_segments_a = return_id(
-        answer, None, 'longest_first', max_sequence_length)
-    
-    return [input_ids_q, input_masks_q, input_segments_q,
-            input_ids_a, input_masks_a, input_segments_a]
+    return [input_ids_claim, input_masks_claim, input_segments_claim,
+            input_ids_evid, input_masks_evid, input_segments_evid]
 
 def compute_input_arrays(df, columns, tokenizer, max_sequence_length):
-    input_ids_q, input_masks_q, input_segments_q = [], [], []
-    input_ids_a, input_masks_a, input_segments_a = [], [], []
-    for _, instance in tqdm(df[columns].iterrows(),total=len(df)):
-        t, q, a = instance.question_title, instance.question_body, instance.answer
+    input_ids_claim, input_masks_claim, input_segments_claim = [], [], []
+    input_ids_evid, input_masks_evid, input_segments_evid = [], [], []
+        
+    for _, row in tqdm(df[columns].iterrows(),total=len(df)):
+        claim, evidence = row.claim, row.evidence
 
         ids_q, masks_q, segments_q, ids_a, masks_a, segments_a = \
         _convert_to_transformer_inputs(t, q, a, tokenizer, max_sequence_length)
